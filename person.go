b74c3712dfce2b76559cda543fe8125aa67df6df
hrantgartley <doyouevenjump@gmail.com>
2023-11-10 00:35:06 -0600

    test commit

--- person.go
+++ person.go
@@ -1,13 +1,31 @@
-package person
+package main
 
 import "fmt"
 
 type Person struct {
-	name    string
-	age     int
-	address string
+	name      string
+	address   string
+	age       int
+	isStudent bool
 }
 
-func person() {
-	fmt.Println("Hello world")
+func newPerson(n string, add string, a int, student bool) *Person {
+	temp := Person{name: n, address: add, age: a, isStudent: student}
+	return &temp
+}
+
+func printPerson(person *Person) {
+	fmt.Println("Name:", person.name)
+	fmt.Println("Address:", person.address)
+	fmt.Println("Age:", person.age)
+	if person.isStudent {
+		fmt.Println("Student?: yes")
+	} else {
+		fmt.Println("Student?: no")
+	}
+}
+
+func main() {
+	person := newPerson("John", "123 Main St", 25, true)
+	printPerson(person)
 }
